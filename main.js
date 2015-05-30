83ed13a211df12122953610a6c0157c62467a5e5
Weston Humphreys <braverun@gmail.com>
2015-05-30 18:58:08 -0400

    Distance between click event and treasure location determanible

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var getRandomNumber = function(size) {
 
 var dimension = 400;
 
+//Generates random numbers for x and y coordinates of treasure
 var target = {
   x: getRandomNumber(dimension),
   y: getRandomNumber(dimension)
@@ -11,7 +12,15 @@ var target = {
 
 var clicks = 0;
 
-$("#map").click(function (event) {
-  clicks++;
-  console.log(clicks);
+$('#map').click(function (event) {
+  clicks++; //Counts clicks
+  getDistance(event, target);
+
 });
+
+
+var getDistance = function(event, target) {
+  var diffX = event.offsetX - target.x;
+  var diffY = event.offsetY - target.y;
+  return Math.sqrt((diffX*diffX) + (diffY*diffY));
+};
