0afc71e7ce4cffd3b53440e45ca84f2dba904be0
Weston Humphreys <braverun@gmail.com>
2015-05-31 14:25:49 -0400

    Game finished *Alert when player finds the treasure *Add comments throughout main.js code

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+
+//Generates random numbers for x and y coordinates of treasure
 var getRandomNumber = function(size) {
   return Math.floor(Math.random() * size);
 };
 
 var dimension = 400;
 
-//Generates random numbers for x and y coordinates of treasure
 var target = {
   x: getRandomNumber(dimension),
   y: getRandomNumber(dimension)
@@ -12,20 +13,33 @@ var target = {
 
 var clicks = 0;
 
+//Runs every time the map is clicked
 $('#map').click(function (event) {
-  clicks++; //Counts clicks
+  //Counts clicks
+  clicks++;
+
   var distance = getDistance(event, target);
   var distanceHint = getDistanceHint(distance);
+
+  //Displays the distant hint in the dom
   $('#distance').text(distanceHint);
+  
+  /*Alerts a message when the distance between the click event and the treasure
+  is less than 8*/
+  if (distance < 8) {
+    alert('You found the treasure in ' + clicks + ' clicks!');
+  }
 });
 
-
+//Finds the distance between the click event and the treasure
 var getDistance = function(event, target) {
   var diffX = event.offsetX - target.x;
   var diffY = event.offsetY - target.y;
   return Math.sqrt((diffX*diffX) + (diffY*diffY));
 };
 
+
+//Sets up messages to be diplayed depending on the distance of the click event from the treasure
 var getDistanceHint = function(distance) {
   if(distance < 10) {
     return 'Boiling Hot!';
