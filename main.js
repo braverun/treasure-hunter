f7628d3967489b0a6e4982c87d24945fb7e08ab1
Weston Humphreys <braverun@gmail.com>
2015-05-31 13:54:37 -0400

    Display distance hint in the dom

--- main.js
+++ main.js
@@ -14,8 +14,9 @@ var clicks = 0;
 
 $('#map').click(function (event) {
   clicks++; //Counts clicks
-  getDistance(event, target);
-
+  var distance = getDistance(event, target);
+  var distanceHint = getDistanceHint(distance);
+  $('#distance').text(distanceHint);
 });
 
 
@@ -23,4 +24,22 @@ var getDistance = function(event, target) {
   var diffX = event.offsetX - target.x;
   var diffY = event.offsetY - target.y;
   return Math.sqrt((diffX*diffX) + (diffY*diffY));
+};
+
+var getDistanceHint = function(distance) {
+  if(distance < 10) {
+    return 'Boiling Hot!';
+  } else if (distance < 20) {
+    return 'Really Hot';
+  } else if (distance < 40) {
+    return 'Hot';
+  } else if (distance < 80) {
+    return 'Warm';
+  } else if (distance < 160) {
+    return 'Cold';
+  } else if (distance < 320) {
+    return 'Really Cold';
+  } else {
+    return 'Freezing!';
+  }
 };
